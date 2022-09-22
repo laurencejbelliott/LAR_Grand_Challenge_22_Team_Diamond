d732c2bb977723e0fded23a08817686ef39bce35
Laurence Roberts-Elliott <laurencejbelliott@gmail.com>
2022-09-22 13:29:41 +0100

    Code from team diamond catkin_ws on Dopey Leo rover

---
+++ navigateWithFiducials/src/moveToGoal.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist, PoseStamped, Pose
+import message_filters
+import math
+import sys
+import tf
+
+class odomPublisher():
+    def __init__(self):
+        self.broadcaster = tf.TransformBroadcaster()
+        self.odomSub = rospy.Subscriber("/wheel_pose", PoseStamped, callback=self.handleOdom)
+
+    def handleOdom(self, msg):
+        self.broadcaster.sendTransform(
+            (msg.pose.position.x, msg.pose.position.y, 0),
+            tf.transformations.quaternion_from_euler(0, 0, msg.pose.orientation.z),
+            rospy.Time.now(),
+            "base_link",
+            "odom"
+        )
+
+class MoveToGoal:
+    def __init__(self):
+        self.odomSub = rospy.Subscriber("/wheel_pose", PoseStamped, callback=self.callback)
+        self.anglePrecision = 0.1
+        self.distancePrecision = 0.1
+        self.twistPub = rospy.Publisher("/cmd_vel", Twist, queue_size=0)
+        self.goalSub = rospy.Subscriber("/goal", Pose, callback=self.setGoal)
+
+        self.goalX = 0
+        self.goalY = 0
+
+        self.forwardRate = 0.3
+        self.turnRate = 0.5
+
+        self.stop = False
+
+    def setGoal(self, msg):
+        self.goalX = msg.position.x 
+        self.goalY = msg.position.y 
+
+    def callback(self, odom):
+        print("callback")
+        odomX = odom.pose.position.x 
+        odomY = odom.pose.position.y 
+
+        angleRequired = math.atan2((self.goalY - odomY), (self.goalX - odomX))
+        
+        quaternion = (odom.pose.orientation.x, odom.pose.orientation.y, odom.pose.orientation.z, odom.pose.orientation.w)
+        angleCurrent = tf.transformations.euler_from_quaternion(quaternion)[2]
+
+        print("Current angle: ", angleCurrent)
+        print("Angle required: ", angleRequired)
+
+        yawResponse = PID(angleCurrent, angleRequired, threshold=self.anglePrecision)
+
+        print("Yaw response: ", yawResponse)
+
+        distance = ((self.goalX - odomX)**2 + (self.goalY - odomY)**2)**0.5
+        print("Distance: ", distance)
+        print("-"*16)
+        if(yawResponse != 0):
+            output = Twist()
+            output.angular.z = yawResponse
+            self.twistPub.publish(output)
+        else:
+            if distance > self.distancePrecision:
+                output = Twist()
+                output.linear.x = min(distance, self.forwardRate)
+                self.twistPub.publish(output)
+        
+
+def PID(actual, goal, threshold=0):
+    pGain = 2.0
+    # iGain = 1
+    # dGain = 1
+
+    error = goal-actual
+    response = pGain * error
+
+    if abs(response) < threshold:
+        return 0
+    else:
+        return response
+    
+
+def main():
+    rospy.init_node("MoveToGoal")
+    mover = MoveToGoal()
+    odomPublisher = OdomPublisher()
+
+    rospy.spin()
+
+if __name__=="__main__":
+    main()
---
+++ publishOdom/src/odomPublisher.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped, Pose, PoseWithCovarianceStamped
+from std_srvs.srv import Empty
+
+class odomPublisher():
+    def __init__(self):
+        self.broadcaster = tf.TransformBroadcaster()
+        self.odomSub = rospy.Subscriber("/fiducial_pose", PoseWithCovarianceStamped, callback=self.handleOdom)
+
+        rospy.wait_for_service("fiducials/clear_map", Empty)
+
+    def handleOdom(self, msg):
+        self.broadcaster.sendTransform(
+            (msg.pose.position.x, msg.pose.position.y, 0),
+            tf.transformations.quaternion_from_euler(0, 0, msg.pose.orientation.z),
+            rospy.Time.now(),
+            "base_link",
+            "odom"
+        )
+
+def main():
+    rospy.init_node("OdomPublisher", anonymous=True)
+    publisher = odomPublisher()
+    print("Running")
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+
+"""
+1) Publish fixed map frame from vidusials
+2) Make odometry relative to the fixed map
+3) Update odometry to remove error when transform from vidusial to base_link is possible
+
+a) publish odometry to [odom] frame
+b) TF from [odom] to [map]
+c) update b) whenever viduasial transform available
+"""
