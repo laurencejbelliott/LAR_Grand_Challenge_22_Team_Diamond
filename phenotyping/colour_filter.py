9eead777c8d814fb5f9a68742cb4c507037de497
Katherine James <kajames@lincoln.ac.uk>
2022-09-23 06:55:16 +0100

    Format and standardisation of naming conventions

--- phenotyping/colour_filter.py
+++ phenotyping/colour_filter.py
@@ -1,5 +1,7 @@
 """
-Grey's colour subscribe, filter and publish script
+LAR Summer Camp 2022
+Maintainer: Katherine James kajames@lincoln.ac.uk
+Class to compute basic phenotyping of crops in the walled garden. Filters using vegetation index ExG, blob detection and use of the depth camera.
 """
 import rospy
 import sys
@@ -10,18 +12,28 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 
-class Looker():
+
+class PhenoProc:
     """Class to implement looking for green, then stopping"""
+
     def __init__(self):
-       
+
         # Subscribe
-        self.camSub = rospy.Subscriber("/d435i_camera/color/image_raw", Image, self.camCallback)
-        self.depthSub = rospy.Subscriber("/d435i_camera/aligned_depth_to_color/image_raw", Image, self.depth_callback)
-        
-        self.camPub = rospy.Publisher("/d435i_camera/color/image_raw/compressed_filtered", Image, queue_size=0)
-        self.depth_pub = rospy.Publisher("/d435i_camera/color/image_raw/depth_filtered", Image, queue_size=0)
-        
-        self.bridge = CvBridge() # Bridge used for converting msgs to cv2 image class
+        self.cam_sub = rospy.Subscriber(
+            "/d435i_camera/color/image_raw", Image, self.cam_callback
+        )
+        self.depth_sub = rospy.Subscriber(
+            "/d435i_camera/aligned_depth_to_color/image_raw", Image, self.depth_callback
+        )
+
+        self.cam_pub = rospy.Publisher(
+            "/d435i_camera/color/image_raw/compressed_filtered", Image, queue_size=0
+        )
+        self.depth_pub = rospy.Publisher(
+            "/d435i_camera/color/image_raw/depth_filtered", Image, queue_size=0
+        )
+
+        self.bridge = CvBridge()  # Bridge used for converting msgs to cv2 image class
 
         # Properties
         self.image = None
@@ -33,47 +45,40 @@ class Looker():
         reply = Image()
         reply.header = msg.header
 
-        depth = self.bridge.imgmsg_to_cv2(msg) # Convert msg to actual cv2 image class (480, 640)
+        depth = self.bridge.imgmsg_to_cv2(
+            msg
+        )  # Convert msg to actual cv2 image class (480, 640)
 
         criteria = (depth < 50000).astype(np.uint8)
         depth = depth * criteria
 
-        self.depth_pub.publish(self.bridge.cv2_to_imgmsg(depth)) 
+        self.depth_pub.publish(self.bridge.cv2_to_imgmsg(depth))
 
-    def camCallback(self, msg):
+    def cam_callback(self, msg):
         # format reply
         reply = Image()
         reply.header = msg.header
 
-        image = self.bridge.imgmsg_to_cv2(msg, "bgr8") # Convert msg to actual cv2 image class       
-        
-        # # HSV THRESH
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Convert to HSV, easier to separate green
-
-        # lowerGreen = np.array([50, 0, 20]) # Lower bounds of detectable green
-        # upperGreen = np.array([70, 128, 230]) # Upper bounds
-        
-        # mask = cv2.inRange(image, lowerGreen, upperGreen) # binary mask, same size as image, 1 indicates a pixel between the bounds
-        # image = cv2.bitwise_and(image, image, mask=mask) # bitwise and to only keep pixels that fit in the filter        
-        # image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+        image = self.bridge.imgmsg_to_cv2(
+            msg, "bgr8"
+        )  # Convert msg to actual cv2 image class
 
         # EXG = 2 * G - B - R
-        blue = image[:,:,0]
-        green = image[:,:,1]
-        red = image[:,:,2]
+        blue = image[:, :, 0]
+        green = image[:, :, 1]
+        red = image[:, :, 2]
 
-        exg = 2 * green - blue - red 
-        exg = np.expand_dims(exg,axis=2)
+        exg = 2 * green - blue - red
+        exg = np.expand_dims(exg, axis=2)
 
-        kernel = np.ones((5,5),np.float32)/25
-        blurred = cv2.filter2D(exg,-1,kernel)
+        kernel = np.ones((5, 5), np.float32) / 25
+        blurred = cv2.filter2D(exg, -1, kernel)
 
-        (T, threshInv) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)     
+        (T, threshInv) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)
         thresh = 255 - threshInv
 
-
-        kernel = np.ones((5,5),np.uint8)
-        thresh = cv2.erode(thresh,kernel,iterations = 1)
+        kernel = np.ones((5, 5), np.uint8)
+        thresh = cv2.erode(thresh, kernel, iterations=1)
 
         params = cv2.SimpleBlobDetector_Params()
         params.filterByArea = True
@@ -81,45 +86,27 @@ class Looker():
         params.filterByCircularity = False
         params.filterByConvexity = False
 
-        
         detector = cv2.SimpleBlobDetector_create(params)
 
-        
-        
-        keypoints = detector.detect(thresh)       
+        keypoints = detector.detect(thresh)
 
-        #out_image = cv2.cvtColor(threshInv, cv2.COLOR_GRAY2BGR) 
-        image = cv2.drawKeypoints(image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        image = cv2.drawKeypoints(
+            image,
+            keypoints,
+            np.array([]),
+            (0, 0, 255),
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+        )
 
+        self.cam_pub.publish(
+            self.bridge.cv2_to_imgmsg(image, "bgr8")
+        )  # Convert image back to bgr8 image msg, then publish
 
-           
-
-        
-
-        # blob detection 
-
-        
-        
-
-
-        # print(image.shape)
-
-        # image = cv2.resize(image, (msg.width/3, msg.height/3)) # resize for sake of fitting on one screen
-        # cv2.namedWindow("Image_" + self.id)
-        # cv2.imshow("Image_" + self.id, cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
-
-        # cv2.waitKey(1)
-
-        self.camPub.publish(self.bridge.cv2_to_imgmsg(image, 'bgr8')) # Convert image back to bgr8 image msg, then publish
-        #self.camPub.publish(self.bridge.cv2_to_imgmsg(self.image)) # Convert image back to bgr8 image msg, then publish
-
-       
-        
 
 if __name__ == "__main__":
     rospy.init_node("pheno")
-    node = Looker()    
-  
+    node = PhenoProc()
+
     print("Filter node running!")
     rospy.spin()
     cv2.destroyAllWindows()
